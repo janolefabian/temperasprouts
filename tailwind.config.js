54fb7ea979b208925297f5848f4b370007e702dc
Jan <jean-eau-le@gmx.net>
2025-07-06 18:04:39 +0200

    git init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ['Avenir', 'sans-serif'],
+      marian: ['"Marian Web 1554"', 'serif']
+    }
+  },
+  plugins: [],
+};
+
